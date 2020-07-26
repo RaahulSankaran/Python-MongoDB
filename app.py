8e6eb80f070689e5a18f6a3370df735778dcec33
RaahulSankaran <30903275+RaahulSankaran@users.noreply.github.com>
2020-07-26 17:14:30 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,59 @@
+from flask import Flask
+
+from flask_pymongo import PyMongo
+
+from flask import jsonify,request
+
+app = Flask(__name__)
+
+app.config['MONGO_DBNAME']='prettyprinted_res'
+app.config['MONGO_URI']= 'mongodb://localhost:27017/testdb'
+
+mongo = PyMongo(app)
+
+@app.route('/testdb', methods=['GET'])
+def get_all_testdb():
+    testdb = mongo.db.testdb
+
+    output = []
+
+    for q in testdb.find():
+        output.append({'name':q['name'],'jobTitle':q['jobTitle'],'emailAddress':q['emailAddress'],'salary':q['salary']})
+
+    return jsonify({'result':output})
+
+@app.route('/testdb/<name>', methods = ['GET'])
+def get_testdb(name):
+    testdb = mongo.db.testdb
+
+    q = testdb.find_one({'name':name})
+    
+    if q:
+        output = {'name':q['name'],'jobTitle':q['jobTitle'],'emailAddress':q['emailAddress'],'salary':q['salary']}
+    else:
+        output = 'No Record'
+    return jsonify({'result':output})
+
+@app.route('/testdb', methods = ['POST'])
+def add_testdb():
+    testdb = mongo.db.testdb
+    name = request.json['name']
+    jobTitle = request.json['jobTitle']
+    emailAddress = request.json['emailAddress']
+    salary = request.json['salary']
+    q = testdb.find()
+
+    if(q['name'] == name):
+        return jsonify({'message': ' name Already Exists'})
+    else:
+        testdb_id = testdb.insert({'name':name,'jobTitle':jobTitle,'emailAddress':emailAddress,'salary':salary})
+        new_testdb = testdb.find_one({'_id':testdb_id})
+        output = {'name':new_testdb['name'],'jobTitle':new_testdb['jobTitle'],'emailAddress':new_testdb['emailAddress'],'salary':new_testdb['salary']}
+        return jsonify({'result': output['name'] + 'Added Sucessfully'})
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
